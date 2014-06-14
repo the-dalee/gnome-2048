5e26634b5bce18cbbd32e423739fb78cbbc7ff21
Damian Lippok <mail.dalee@gmail.com>
2014-06-14 19:56:04 +0200

    Locales dir path fixed

--- gnome-2048.py
+++ gnome-2048.py
@@ -3,6 +3,7 @@ from gui import main
 import os
 from gui.main import Gnome2048Application
 import sys
+import i18n
 
 application = Gnome2048Application()
 exit_status = application.run(sys.argv)
--- i18n.py
+++ i18n.py
@@ -1,10 +1,19 @@
 import gettext
 import locale
+from argparse import ArgumentError
+import os
 
-current_locale, encoding = locale.getdefaultlocale()
+locale.setlocale(locale.LC_ALL, '')
+current_locale, encoding = locale.getlocale()
 locale.bindtextdomain('gnome-2048', 'locales')
-t = gettext.translation('gnome-2048', 'locales/', 
+
+exec_path = os.path.realpath(__file__)
+exec_dir = os.path.dirname(exec_path)
+locales_dir = os.path.join(exec_dir, "locales", "")
+        
+t = gettext.translation('gnome-2048', locales_dir, 
                         [current_locale], fallback=True)
+
 t.install()
 _ = t.gettext
 __ = t.ngettext
