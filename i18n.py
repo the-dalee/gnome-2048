5e8906280e2bad24078340a1eb10145d5033ed1e
Damian Lippok <mail.dalee@gmail.com>
2014-06-14 20:04:01 +0200

    Locale dir for glade translation domain fixed

--- i18n.py
+++ i18n.py
@@ -2,15 +2,15 @@ import gettext
 import locale
 from argparse import ArgumentError
 import os
-
-locale.setlocale(locale.LC_ALL, '')
-current_locale, encoding = locale.getlocale()
-locale.bindtextdomain('gnome-2048', 'locales')
-
 exec_path = os.path.realpath(__file__)
 exec_dir = os.path.dirname(exec_path)
+locale.setlocale(locale.LC_ALL, '')
 locales_dir = os.path.join(exec_dir, "locales", "")
-        
+
+current_locale, encoding = locale.getlocale()
+
+locale.bindtextdomain('gnome-2048', locales_dir)
+
 t = gettext.translation('gnome-2048', locales_dir, 
                         [current_locale], fallback=True)
 
