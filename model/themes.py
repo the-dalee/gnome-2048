ab69a7ade71261300568f70e93faa4d88f714530
Damian Lippok <mail.dalee@gmail.com>
2014-08-02 23:26:48 +0200

    Theme attributes and corresponding model class added

---
+++ model/themes.py
@@ -0,0 +1,37 @@
+import locale
+
+
+class ThemeAttributes(object):
+    def __init__(self):
+        self.path = ""
+        self.localized_name = {"en": "Unknown"}
+        self.localized_description = {"en": "Description not specified"}
+        self.localized_license = {"en": "All right reserved"}
+        self.copyright = "Copyright © 2014"
+
+    def get_description(self):
+        return self.get_localized(self.localized_description)
+
+    def get_name(self):
+        return self.get_localized(self.localized_name)
+
+    def get_license(self):
+        return self.get_localized(self.localized_name)
+
+    description = property(get_description)
+    name = property(get_name)
+    license = property(get_license)
+
+    def get_localized(self, translations):
+        language = locale.getlocale()[0]
+
+        if language in translations:
+            return translations[language]
+        elif "en" in translations:
+            return translations["en"]
+        elif "en_US" in translations:
+            return translations["en_US"]
+        elif "en_UK" in translations:
+            return translations["en_UK"]
+        else:
+            return dict.popitem()[1]
